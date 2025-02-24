6d8fb28a19d7c4327148c3053560a40fce05eca2
Supian M <supianidz@gmail.com>
2025-02-25 02:48:31 +0800

    refactor: optimize component by importing only necessary icons

--- src/views/commons/vue/Icon.vue
+++ src/views/commons/vue/Icon.vue
@@ -1,23 +1,33 @@
 <template>
-  <component :is="icon" :stroke-width="stroke"/>
+    <component :is="icon" :stroke-width="stroke" />
 </template>
 
 <script lang="ts" setup>
+import { AtSign, Facebook, Github, Instagram, Linkedin, Moon, Sun } from 'lucide-vue-next';
 import { computed } from 'vue';
-import * as icons from 'lucide-vue-next';
 
 const props = defineProps({
-  name: {
-    type: String,
-    required: true
-  },
-  stroke: {
-    type: Number,
-    default: 2
-  },
+    name: {
+        type: String,
+        required: true
+    },
+    stroke: {
+        type: Number,
+        default: 2
+    }
 });
 
+const icons = {
+    Moon,
+    Sun,
+    Facebook,
+    Instagram,
+    AtSign,
+    Linkedin,
+    Github
+};
+
 const icon = computed(() => {
-  return (icons as Record<string, unknown>)[props.name];
+    return icons[props.name as keyof typeof icons];
 });
 </script>
