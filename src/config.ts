18f8a8921adf4db0f1097716823fd28b1ab6710b
Supian M <supianidz@gmail.com>
2025-02-25 04:22:08 +0800

    chore: convert image assets to WebP

--- src/config.ts
+++ src/config.ts
@@ -80,27 +80,27 @@ export const experiences: Array<Experience> = [
         role: 'Back-End Engineer',
         comp: 'Procurement of Goods and Services Work Unit of Hulu Sungai Selatan Regency',
         desc: 'Developed and managed a marketplace for transactions between local governments and SMEs, facilitated adoption by other districts, and integrated with Bela Pengadaan to streamline procurement processes.',
-        logo: '/images/company/4.png'
+        logo: '/images/company/4.webp'
     },
     {
         date: 'August 2022 · December 2023',
         role: 'Full-Stack Engineer',
         comp: 'Regional Water Utility Company Intan Banjar',
         desc: 'Building and optimizing systems for clean water payment processing, installation requests, and customer complaint management, ensuring seamless and efficient service delivery.',
-        logo: '/images/company/3.png'
+        logo: '/images/company/3.webp'
     },
     {
         date: 'February 2020 · December 2022',
         role: 'Full-Stack Engineer',
         comp: 'Department of Communication and Informatics of Hulu Sungai Selatan Regency',
         desc: 'Responsible for developing government applications and managing main and email servers infrastructures to ensure seamless operations and robust security.',
-        logo: '/images/company/2.png'
+        logo: '/images/company/2.webp'
     },
     {
         date: 'February 2019 · Feb 2020',
         role: 'Administrator & Full-Stack Programmer',
         comp: 'Hasnur Jaya International',
         desc: 'Freelance Developer focused on Warehouse Management Systems, streamlining inventory processes with FIFO and LIFO strategies, overseeing fuel and clean water data management, and automating asset maintenance schedules.',
-        logo: '/images/company/1.png'
+        logo: '/images/company/1.webp'
     }
 ];
