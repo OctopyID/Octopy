3c0257100e7ce856a8134f06c7628cc0df32a359
Supian M <supianidz@gmail.com>
2025-03-01 21:12:07 +0800

    chore: update badge

--- src/views/layouts/partials/vue/Footer.vue
+++ src/views/layouts/partials/vue/Footer.vue
@@ -2,7 +2,7 @@
   <section
     class="mt-auto flex w-full flex-col gap-y-3 border-t border-gray-200 bg-white py-8 text-gray-700 sm:mt-20 dark:border-gray-800 dark:bg-gray-900"
   >
-    <div class="container mx-auto flex max-w-6xl flex-col items-center sm:flex-row">
+    <div class="container mx-auto flex max-w-5xl flex-col items-center sm:flex-row">
       <Logo />
       <p
         class="mt-4 text-sm text-gray-700 sm:mt-0 sm:ml-4 sm:border-l sm:border-gray-300 sm:pl-4 dark:text-gray-100 dark:sm:border-gray-400"
@@ -23,7 +23,7 @@
       </div>
     </div>
 
-    <div class="container mx-auto flex w-full max-w-6xl flex-row items-center justify-center gap-x-4 xl:justify-end">
+    <div class="container mx-auto flex w-full max-w-5xl flex-row items-center justify-center gap-x-4 xl:justify-end">
       <div class="flex flex-row gap-x-1 text-sm">
         <template v-for="(link, index) in links" :key="link.route">
           <a :href="link.route" class="text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white">
