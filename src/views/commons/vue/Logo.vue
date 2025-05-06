618b4d96539a8ed2954c0e7ac84c52cea50902de
Supian M <supianidz@gmail.com>
2025-05-07 01:55:09 +0800

    style(logo): adjust spacing and font size in Logo component

--- src/views/commons/vue/Logo.vue
+++ src/views/commons/vue/Logo.vue
@@ -1,11 +1,11 @@
 <template>
   <a
     href="/"
-    class="group relative z-30 flex h-5 items-center space-x-1 text-base font-semibold text-black dark:text-white"
+    class="group relative z-30 flex h-5 items-center space-x-1.5 text-base font-semibold text-black dark:text-white"
   >
     <span class="duration-300 ease-in-out group-hover:scale-[1.2] group-hover:-rotate-12">
       <img src="/images/octopy.webp" class="h-12" alt="Octopy ID">
     </span>
-    <span class="-translate-y-0.5"> Octopy ID</span>
+    <span class="-translate-y-0.5 text-xl"> Octopy ID</span>
   </a>
 </template>
