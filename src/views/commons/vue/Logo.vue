72137cece0e331f99ab0da8c10f396d59dba9e02
Supian M <supianidz@gmail.com>
2025-05-07 12:57:25 +0800

    fix: adjust logo image height

--- src/views/commons/vue/Logo.vue
+++ src/views/commons/vue/Logo.vue
@@ -4,7 +4,7 @@
     class="relative z-30 flex h-5 items-center space-x-1.5 text-base font-semibold text-black dark:text-white"
   >
     <span class="duration-300 ease-in-out">
-      <img src="/images/octopy.webp" class="h-12" alt="Octopy ID" />
+      <img src="/images/octopy.webp" class="h-10" alt="Octopy ID" />
     </span>
     <span class="-translate-y-0.5 text-xl"> Octopy ID</span>
   </a>
